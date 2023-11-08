fe61fb5cb29d1e7664b5addfd73499d8f2a18529
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-08 08:43:00 +0700

    Customer Sebelum Update

---
+++ Modul5/src/Customer.java
@@ -0,0 +1,14 @@
+
+
+/**
+ *
+ * @author Rolan Firmansyah
+ */
+public class Customer {
+    class DtaRcrd102 {
+private Date genymdhms;
+private Date modymdhms;
+private final String pszqint = "102";
+/* ... */
+}
+}
