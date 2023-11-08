20830966af7e37e1670de2b5681760b33114473f
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-08 08:12:22 +0700

    Sebelum Update

---
+++ Modul5/src/modul5/Minesweeper.java
@@ -0,0 +1,16 @@
+
+package modul5;
+
+/**
+ *
+ * @author Rolan Firmansyah
+ */
+public class Minesweeper {
+    public list<int[]> getHem(){
+        List<int[]> list1 = new ArrayList<int[]>(); 
+        for (int[] x : thelist)
+            if (x[0] == 4)
+                list.add(x);
+        return list1;
+    }
+}
