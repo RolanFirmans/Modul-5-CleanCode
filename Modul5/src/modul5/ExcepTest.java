e2064e0b84beae074634b2823307136a2f897bfd
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-12 01:10:06 +0700

    ExcepTest

---
+++ Modul5/src/modul5/ExcepTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul5;
+
+/**
+ *
+ * @author Rolan Firmansyah
+ */
+import java.io.*;
+public class ExcepTest {
+    public static void main(String args[]) {
+        try {
+        int a[] = new int[2];
+            System.out.println("Access element three :" + a[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception thrown :" + e);
+        }
+        System.out.println("Out of the block");
+    }
+}
