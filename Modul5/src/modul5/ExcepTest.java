8bf696b7f07c34794c85a11b207b42e79560ff30
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-12 01:49:14 +0700

    The Finally Block ExcepTest.java

--- Modul5/src/modul5/ExcepTest.java
+++ Modul5/src/modul5/ExcepTest.java
@@ -8,26 +8,15 @@ package modul5;
 import java.io.*;
 public class ExcepTest {
     public static void main(String args[]) {
+        int a[] = new int[2];
         try {
-            int a[] = new int[2];
             System.out.println("Access element three :" + a[3]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Exception thrown :" + e);
+        }finally {
+        a[0] = 6;
+            System.out.println("First element value: " + a[0]);
+            System.out.println("The finally statement is executed");
         }
-            System.out.println("Out of the block");
-            FileInputStream file = null;
-            byte x;
-        String fileName = "example.txt"; // Ganti dengan nama file yang sesuai
-        try {
-            file = new FileInputStream(fileName);
-            x = (byte) file.read();
-        } catch (FileNotFoundException f) {
-            f.printStackTrace();
-            return -1;
-        } catch (IOException i) {
-            i.printStackTrace();
-            return -1;
-        }
-        System.out.println("Out of the file handling block");
-    }
+   }
 }
