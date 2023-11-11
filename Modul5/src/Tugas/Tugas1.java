017d949a6614e5cc68e1eec415d3a9ee1e53b754
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-12 02:04:29 +0700

    Tugas sebelum di update

---
+++ Modul5/src/Tugas/Tugas1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas;
+
+/**
+ *
+ * @author Rolan Firmansyah
+ */
+public class Tugas1 {
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) {
+    System.out.println(3/0);
+    }
+}
