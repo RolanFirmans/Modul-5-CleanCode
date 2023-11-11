36d8b2bd631706b595c0c53ef7914bbdef9450db
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-12 02:59:30 +0700

    Sesudah Di Update

--- Modul5/src/Tugas/ListOfNumbers2.java
+++ Modul5/src/Tugas/ListOfNumbers2.java
@@ -9,7 +9,9 @@ package Tugas;
  * @author Rolan Firmansyah
  */
 
-import java.io.*;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.FileNotFoundException;
 import java.util.Vector;
 
 public class ListOfNumbers2 {
@@ -20,24 +22,35 @@ public class ListOfNumbers2 {
     public ListOfNumbers2() {
         victor = new Vector(size);
         for (int i = 0; i < size; i++) {
-            victor.addElement(new Integer(i));
-        }
-        this.readList("infile.txt");
-        this.writeList();
-    }
-
-    public void readList(String fileName) {
-        String line = null;
-        RandomAccessFile raf = new RandomAccessFile(fileName, "r");
-        while ((line = raf.readLine()) != null) {
-            Integer i = new Integer(Integer.parseInt(line));
-            System.out.println(i);
             victor.addElement(i);
         }
+        try {
+            this.readList("infile.txt");
+            this.writeList();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
     }
-    //tambahkan exception jika file tidak ditemukan
-    //tambahkan exception untuk output error
+
+    public void readList(String fileName) throws IOException {
+        String line = null;
+        try (RandomAccessFile raf = new RandomAccessFile(fileName, "r")) {
+            while ((line = raf.readLine()) != null) {
+                Integer i = Integer.parseInt(line);
+                System.out.println(i);
+                victor.addElement(i);
+            }
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            throw new IOException("Error reading the file: " + e.getMessage());
+        }
     }
+
     public void writeList() {
+        // Method implementation for writing the list
+    }
 }
 
