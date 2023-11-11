c8988c5f37891ab365277a6db06a1d62c1b355c9
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-12 01:51:34 +0700

    Sebelum di update

---
+++ Modul5/src/Tugas/kelasPengguna.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas;
+
+/**
+ *
+ * @author Rolan Firmansyah
+ */
+public class kelasPengguna {
+    protected String n; //username
+    protected String p; //password
+    public void penyimpanan(String n, String p) {
+    // isi code
+    }
+}
