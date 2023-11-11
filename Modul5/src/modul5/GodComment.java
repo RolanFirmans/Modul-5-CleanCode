a41feb8fdee6690027f9ae9fa7e502a41568dc24
Rolan Firmansyah <rolanfirmansyah1@gmail.com>
2023-11-11 17:42:09 +0700

    Legal Comments

---
+++ Modul5/src/modul5/GodComment.java
@@ -0,0 +1,12 @@
+
+package modul5;
+
+/**
+ *
+ * @author Rolan Firmansyah
+ */
+public class GodComment {
+    // Copyright (C) 2011 by Osoco. All rights reserved.
+    // Released under the terms of the GNU General Public License
+    // version 2 or later. 
+}
